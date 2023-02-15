f6d84031f371aed4a7fd1bfe2da68e5db0790101
dizzie <dizziefalls@gmail.com>
2023-02-15 16:50:45 -0600

    Sets up react and tailwind placeholders

---
+++ README.md
@@ -0,0 +1,15 @@
+Example fullstack prisma app as described on their blog
+```
+https://www.prisma.io/blog/e2e-type-safety-graphql-react-1-I2GxIfxkSZ
+```
+
+Uses:
+- Prisma
+- Postgres
+- Typescript
+- GraphQL Yoga
+- Pothos
+- Vite
+- React
+- GraphQL Codegen
+- TailwindCSS
---
+++ react-client/src/components/UserDisplay.tsx
@@ -0,0 +1,27 @@
+import { User } from "../types"
+import MessageDisplay from "./MessageDisplay"
+
+type Props = {
+  user: User
+}
+
+function UserDisplay({ user }: Props) {
+  return (
+    <div className="flex gap-x-24 justify-center">
+      <div className="rounded-sm flex justify-center items-center drop-shadow-md bg-neutral-700 w-48 h-20">
+        <p className="text-xl text-gray-200 font-bold">
+          {user.name}
+        </p>
+      </div>
+      <div>
+        {
+          user.messages.map((message, i) => {
+            return <MessageDisplay message={message} index={i} key={i} />
+          })
+        }
+      </div>
+    </div>
+  )
+}
+
+export default UserDisplay

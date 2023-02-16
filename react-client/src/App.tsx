a0a65cfa3cc1b7b638e2e498c140b65053331b65
dizzie <dizziefalls@gmail.com>
2023-02-16 00:13:31 -0600

    Generates graphQL types and integrates API in react

--- react-client/src/App.tsx
+++ react-client/src/App.tsx
@@ -1,20 +1,16 @@
-import { User } from "./types"
 import UserDisplay from "./components/UserDisplay"
+import { useQuery } from "urql"
+import { GetUsersDocument } from "./graphql/generated"
 
 function App() {
-  const users: User[] = [{
-    name: 'Prisma Fan',
-    messages: [{
-      body: 'Prisma Rocks!!'
-    }, {
-      body: 'Did I mention I love Prisma? So they say...'
-    }]
-  }]
+  const [results] = useQuery({
+    query: GetUsersDocument
+  })
 
   return ( 
     <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
       {
-        users.map((user, i) => <UserDisplay user={user} key={i}/>)
+        results.data?.users.map((user, i) => <UserDisplay user={user} key={i}/>)
       }
     </div>
   )
